a48ddc496f0c7fb729f635a4e3e8be920c0e37ae
SHIVRAJSHINDE <30665232+SHIVRAJSHINDE@users.noreply.github.com>
2020-04-21 19:41:28 +0530

    2nd Commit, Practice ML

---
+++ AShiv/my_pac/arith.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 14:43:12 2019
+
+@author: HP
+"""
+
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def multi(x,y):
+    return x*y
+def divi(x,y):
+    return x/y
